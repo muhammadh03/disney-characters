48ee75e5e42ecd37d6bc4aebd72b49634593cf2e
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2024-08-05 07:21:34 +0400

    created disney charactor pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from'react';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  const [page, setPage] = useState(1);
+
+  const getCharacters = async () => {
+    const response = await fetch(`https://api.disneyapi.dev/character?pageSize=5&page=${page}`);
+    const {data} = await response.json();
+    setCharacters(data);
+  };
+
+  useEffect(() => {
+    getCharacters();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div 
+      className="App" 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        height: '100vh',
+        }}>
+        {characters.map(({name, imageUrl}) => (
+          <img key = {name} src={imageUrl} alt={name} style={{margin: '10px'}} />
+        ))}
+      </div>
+      <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        }}
+      >
+      <button disabled={page === 1} onClick={() => {
+        setPage(page - 1);
+      }}>Prev</button>
+      <button onClick={() => {
+        setPage(page + 1);
+      }}>Next</button>
+      </div>
+    </>
   );
 }
 
